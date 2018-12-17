86b3a9d1902db30426787c90acd113cf50374746
Henrik Åhl <henrikaahl@gmail.com>
2018-12-17 11:38:44 +0000

    added external packages

--- code/external/setup.py
+++
@@ -1,7 +0,0 @@
-# setup.py
-# Usage: ``python setup.py build_ext --inplace``
-from distutils.core import setup, Extension
-import numpy
-setup(name='_psf',
-	ext_modules=[Extension('_psf', ['psf.c'],
-	include_dirs=[numpy.get_include()])])
